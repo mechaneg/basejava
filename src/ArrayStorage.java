baf2350094e7236401375790321384d4dc0c6a0b
shapds <dims1001@yandex.ru>
2019-02-18 11:41:00 +0300

    hw2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,60 +5,82 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
 
-  private final int maxSize = 10000;
-  private Resume[] storage = new Resume[maxSize];
-  private int curSize = 0;
+    private static final int MAX_SIZE = 10000;
+    private Resume[] storage = new Resume[MAX_SIZE];
+    private int curSize = 0;
 
-  public void clear() {
-    Arrays.fill(storage, null);
-    curSize = 0;
-  }
-
-  public void save(Resume resume) {
-    if (resume == null) {
-      throw new IllegalArgumentException("resume is null");
-    }
-    if (curSize == maxSize) {
-      throw new StorageOverflowException();
+    private boolean isInStorage(String uuid) {
+        return Arrays.stream(storage, 0, curSize).anyMatch(
+            element -> element.uuid.equals(uuid)
+        );
     }
 
-    storage[curSize] = resume;
-    curSize++;
-  }
-
-  Resume get(String uuid) {
-    for (int i = 0; i < curSize; i++) {
-      if (storage[i].uuid.equals(uuid)) {
-        return storage[i];
-      }
+    public void clear() {
+        Arrays.fill(storage, 0, curSize, null);
+        curSize = 0;
     }
-    return null;
-  }
 
-  void delete(String uuid) {
-    for (int i = 0; i < curSize; i++) {
-      if (storage[i].uuid.equals(uuid)) {
-        storage[i] = storage[curSize - 1];
-        storage[curSize - 1] = null;
-        curSize--;
-        return;
-      }
+    public void save(Resume resume) {
+        if (resume == null) {
+            throw new IllegalArgumentException("resume is null");
+        }
+        if (isInStorage(resume.uuid)) {
+            throw new DuplicateElementException();
+        }
+        if (curSize == MAX_SIZE) {
+            throw new StorageOverflowException();
+        }
+
+        storage[curSize] = resume;
+        curSize++;
     }
-  }
 
-  /**
-   * @return array, contains only Resumes in storage (without null)
-   */
-  Resume[] getAll() {
-    return Arrays.copyOf(storage, curSize);
-  }
+    public Resume get(String uuid) {
+        for (int i = 0; i < curSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
+        throw new AbsentElementException();
+    }
 
-  int size() {
-    return curSize;
-  }
+    public void delete(String uuid) {
+        for (int i = 0; i < curSize; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[curSize - 1];
+                storage[curSize - 1] = null;
+                curSize--;
+                return;
+            }
+        }
+        throw new AbsentElementException();
+    }
+
+    public void update(Resume resume) {
+        for (int i = 0; i < curSize; i++) {
+            if (storage[i].uuid.equals(resume.uuid)) {
+                storage[i] = resume;
+                return;
+            }
+        }
+        throw new AbsentElementException();
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, curSize);
+    }
+
+    public int size() {
+        return curSize;
+    }
 
     /**
      * ArrayStorage specific exceptions
      */
     public class StorageOverflowException extends RuntimeException {}
+    public class DuplicateElementException extends RuntimeException {}
+    public class AbsentElementException extends RuntimeException {}
 }
