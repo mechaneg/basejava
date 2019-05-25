e7212c674c6961a46c8e4ec01db9a85293202e2e
shapds <dims1001@yandex.ru>
2019-05-25 14:26:28 +0300

    hw9: prettify output of project files structure

--- src/DisplayFiles.java
+++ src/DisplayFiles.java
@@ -5,17 +5,17 @@ public class DisplayFiles {
 
     public static void main(String... args) {
         File projectRoot = new File(".");
-        displayFiles(projectRoot);
+        displayFiles(projectRoot, "");
     }
 
-    private static void displayFiles(File directory) {
-        for (File file : Objects.requireNonNull(directory.listFiles())) {
-            if (file.isFile() && !file.isHidden()) {
-                System.out.println(file.getName());
-            }
-            if (file.isDirectory() && !file.isHidden()) {
-                displayFiles(file);
-            }
+    private static void displayFiles(File rootDir, String blankPrefix) {
+        System.out.println(blankPrefix + rootDir.getName() + ":");
+
+        for (File file : Objects.requireNonNull(rootDir.listFiles(file -> file.isFile() && !file.isHidden()))) {
+            System.out.println(blankPrefix + " -" + file.getName());
+        }
+        for (File dir : Objects.requireNonNull(rootDir.listFiles(file -> file.isDirectory() && !file.isHidden()))) {
+            displayFiles(dir, blankPrefix + "  ");
         }
     }
 }
